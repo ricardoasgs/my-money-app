cb1a0f47ceef89054d64e368c1bdd0fe2d5e1261
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2018-11-05 11:53:04 -0200

    Ajuste para pegar o sumário de cada usuário

--- src/Actions/dashboardActions.js
+++ src/Actions/dashboardActions.js
@@ -3,7 +3,7 @@ import { toastr } from "react-redux-toastr";
 import { BILLING_SUMMARY_FETCHED, API_URL } from "../Config/constants";
 
 export function getSummary(id) {
-  const request = axios.get(`${API_URL}/billingCycles/summary?userId=${id}`);
+  const request = axios.get(`${API_URL}/billingCycles/summary/${id}`);
   return {
     type: BILLING_SUMMARY_FETCHED,
     payload: request
